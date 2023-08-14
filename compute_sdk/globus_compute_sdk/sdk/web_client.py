29989ef52dddc0d987d04527987c8b65f5c3ac0d
Alok Kamatar <alokvk2@uchicago.edu>
2023-08-14 16:10:21 -0500

    Switching to new interface (again)

--- compute_sdk/globus_compute_sdk/sdk/web_client.py
+++ compute_sdk/globus_compute_sdk/sdk/web_client.py
@@ -147,9 +147,9 @@ class WebClient(globus_sdk.BaseClient):
     def submit(
         self, endpoint_id: UUID_LIKE_T, batch: t.Dict[str, t.Any]
     ) -> globus_sdk.GlobusHTTPResponse:
-        batch["endpoint_id"] = str(endpoint_id)
-        return self.post(f"/v3/submit", data=batch)
-        # return self.post(f"/v3/endpoints/{endpoint_id}/submit", data=batch)
+        # batch["endpoint_id"] = str(endpoint_id)
+        # return self.post(f"/v3/submit", data=batch)
+        return self.post(f"/v3/endpoints/{endpoint_id}/submit", data=batch)
 
     def register_endpoint(
         self,
